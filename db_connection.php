c22c56c987d669cafeb8b01894e2d6d2fdcf727e
1Kr3ksu <160737493+1Kr3ksu@users.noreply.github.com>
2025-01-24 12:46:44 +0100

    Delete db_connection.php

--- db_connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "filmy");
-if ($conn->connect_error) {
-    die("Połączenie nieudane: " . $conn->connect_error);
-    echo "Error ".$conn->connect_error;
-}
-?>
